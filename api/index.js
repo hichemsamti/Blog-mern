f7d4f1b0aadec41a78888f7ab943970b9bea7330
hichemsamti <samtihichem@gmail.com>
2021-06-11 12:24:17 +0100

    finished design started api

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const dotenv= require("dotenv")
+const mongoose= require ("mongoose")
+
+
+
+
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(console.log("Connected to DB")).catch(err=>console.log(err))
+
+app.listen("5000",()=>{
+    console.log("server is connected on port 5000")
+})
