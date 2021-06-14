85ec18736c5c4671eb41c06844bf4176e0666fdc
hichemsamti <samtihichem@gmail.com>
2021-06-14 14:45:25 +0100

    finished api

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const mongoose= require ("mongoose")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const postRoute = require('./routes/posts')
+const categoryRoute = require("./routes/categories")
+const multer = require ("multer")
 
 
 
@@ -22,9 +24,31 @@ mongoose.connect(process.env.MONGO_URL,{
 }).then(console.log("Connected to DB")).catch(err=>console.log(err))
 
 
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"images")},
+    
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name)},
+    
+
+
+
+})
+
+const upload = multer({storage:storage})
+app.post("/api/upload", upload.single("file"),(req,res)=>{
+    res.status(200).json("file uploaded")
+
+
+
+})
+
+
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/posts",postRoute)
+app.use("/api/categories", categoryRoute)
 
 
 
